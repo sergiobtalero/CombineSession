6af946c839f752aeefa046d68603de23a421030d
Sergio Bravo <sergiobtalero@aol.com>
2023-01-20 08:59:53 -0500

    Initial Commit

---
+++ CombineExpressSession/CombineExpressSessionApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineExpressSessionApp.swift
+//  CombineExpressSession
+//
+//  Created by Sergio Bravo Talero on 20/01/23.
+//
+
+import SwiftUI
+
+@main
+struct CombineExpressSessionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
